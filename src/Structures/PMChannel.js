2a4b892743e55bfcf5c2080ed52615ee10f22913
macdja38 <jakeincanada@icloud.com>
2017-01-31 00:52:05 -0500

    log pm channel creation

--- src/Structures/PMChannel.js
+++ src/Structures/PMChannel.js
@@ -6,82 +6,83 @@ import Cache from "../Util/Cache";
 import {reg} from "../Util/ArgumentRegulariser";
 
 export default class PMChannel extends Channel {
-	constructor(data, client){
-		super(data, client);
+  constructor(data, client) {
+    super(data, client);
 
-		this.type = data.type;
-		this.lastMessageID = data.last_message_id || data.lastMessageID;
-		this.messages = new Cache("id", client.options.maxCachedMessages);
-        if(data.recipients instanceof Cache) {
-        	this.recipients = data.recipients;
-        } else {
-			this.recipients = new Cache();
-        	data.recipients.forEach((recipient) => {
-	            this.recipients.add(this.client.internal.users.add(new User(recipient, this.client)));
-	        });
-        }
-        this.name = data.name !== undefined ? data.name : this.name;
-        this.owner = data.owner || this.client.internal.users.get("id", data.owner_id);
-        this.icon = data.icon !== undefined ? data.icon : this.icon;
-	}
+    console.log(data);
+    this.type = data.type;
+    this.lastMessageID = data.last_message_id || data.lastMessageID;
+    this.messages = new Cache("id", client.options.maxCachedMessages);
+    if (data.recipients instanceof Cache) {
+      this.recipients = data.recipients;
+    } else {
+      this.recipients = new Cache();
+      data.recipients.forEach((recipient) => {
+        this.recipients.add(this.client.internal.users.add(new User(recipient, this.client)));
+      });
+    }
+    this.name = data.name !== undefined ? data.name : this.name;
+    this.owner = data.owner || this.client.internal.users.get("id", data.owner_id);
+    this.icon = data.icon !== undefined ? data.icon : this.icon;
+  }
 
-	get recipient() {
-		return this.recipients[0];
-	}
+  get recipient() {
+    return this.recipients[0];
+  }
 
-	/* warning! may return null */
-	get lastMessage(){
-		return this.messages.get("id", this.lastMessageID);
-	}
+  /* warning! may return null */
+  get lastMessage() {
+    return this.messages.get("id", this.lastMessageID);
+  }
 
-	toString(){
-		return this.recipient.toString();
-	}
+  toString() {
+    return this.recipient.toString();
+  }
 
-	toObject() {
-		let keys = ['type', 'lastMessageID', 'recipient'],
-			obj = {};
+  toObject() {
+    let keys = ['type', 'lastMessageID', 'recipient'],
+      obj = {};
 
-		for (let k of keys) {
-			obj[k] = this[k];
-		}
+    for (let k of keys) {
+      obj[k] = this[k];
+    }
 
-		return obj;
-	}
+    return obj;
+  }
 
-	sendMessage(){
-		return this.client.sendMessage.apply(this.client, reg(this, arguments));
-	}
+  sendMessage() {
+    return this.client.sendMessage.apply(this.client, reg(this, arguments));
+  }
 
-	send() {
-		return this.client.sendMessage.apply(this.client, reg(this, arguments));
-	}
+  send() {
+    return this.client.sendMessage.apply(this.client, reg(this, arguments));
+  }
 
-	sendTTSMessage(){
-		return this.client.sendTTSMessage.apply(this.client, reg(this, arguments));
-	}
+  sendTTSMessage() {
+    return this.client.sendTTSMessage.apply(this.client, reg(this, arguments));
+  }
 
-	sendTTS() {
-		return this.client.sendTTSMessage.apply(this.client, reg(this, arguments));
-	}
+  sendTTS() {
+    return this.client.sendTTSMessage.apply(this.client, reg(this, arguments));
+  }
 
-	sendFile() {
-		return this.client.sendFile.apply(this.client, reg(this, arguments));
-	}
+  sendFile() {
+    return this.client.sendFile.apply(this.client, reg(this, arguments));
+  }
 
-	startTyping() {
-		return this.client.startTyping.apply(this.client, reg(this, arguments));
-	}
+  startTyping() {
+    return this.client.startTyping.apply(this.client, reg(this, arguments));
+  }
 
-	stopTyping() {
-		return this.client.stopTyping.apply(this.client, reg(this, arguments));
-	}
+  stopTyping() {
+    return this.client.stopTyping.apply(this.client, reg(this, arguments));
+  }
 
-	getLogs() {
-		return this.client.getChannelLogs.apply(this.client, reg(this, arguments));
-	}
+  getLogs() {
+    return this.client.getChannelLogs.apply(this.client, reg(this, arguments));
+  }
 
-	getMessage() {
-		return this.client.getMessage.apply(this.client, reg(this, arguments));
-	}
+  getMessage() {
+    return this.client.getMessage.apply(this.client, reg(this, arguments));
+  }
 }
